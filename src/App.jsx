f5852712875b0ec8630128b8ad54c74ebaf1bf1b
Othmane Khiter <othmane.khiter@gmail.com>
2022-02-02 16:16:44 +0100

    Initialized the wiki project with components and pages folders, and established routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Creatures from './pages/Creatures';
+import Equipments from './pages/Equipments';
+import Materials from './pages/Materials';
+import Monsters from './pages/Monsters';
+import Treasures from './pages/Treasures';
+
 function App() {
-  return <h1>Hello there!</h1>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/faune" element={<Creatures />} />
+        <Route path="/equipements" element={<Equipments />} />
+        <Route path="/matériaux" element={<Materials />} />
+        <Route path="/bestiaire" element={<Monsters />} />
+        <Route path="/minerais" element={<Treasures />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <h1>Accueil</h1>
+      <h2>Actualités</h2>
+      <h2>Vidéos</h2>
+    </>
+  );
+}
